cd7e90325e456e614dc1779135792f041ed6ab00
Jonathan Hellwig <jonathan.hellwig@posteo.de>
2024-01-02 12:08:42 +0100

    Qualify test import

--- test/runtests.jl
+++ test/runtests.jl
@@ -73,26 +73,26 @@ argument_variables = Set{Tuple{Symbol, Symbol}}([(:x, :Real), (:y, :Real)])
 
 
 
-@test collect_assumptions(function_definition) == assumptions
-@test parse_arguments(function_definition) == argument_variables
-@test collect_assertions(function_definition) == assertions
-@test remove_assertions(body) == body_without_assertions
-@test remove_assumptions(body) == body_without_assumption
-@test collect_unique_variables(function_definition) == body_variables
+@Test.test collect_assumptions(function_definition) == assumptions
+@Test.test parse_arguments(function_definition) == argument_variables
+@Test.test collect_assertions(function_definition) == assertions
+@Test.test remove_assertions(body) == body_without_assertions
+@Test.test remove_assumptions(body) == body_without_assumption
+@Test.test collect_unique_variables(function_definition) == body_variables
 
 
 result = parse_body(function_definition)
-@test result[1] == body_variables
-@test result[2] == body_wihout_assumptions_and_assertions
-@test result[3] == assumptions
-@test result[4] == assertions
+@Test.test result[1] == body_variables
+@Test.test result[2] == body_wihout_assumptions_and_assertions
+@Test.test result[3] == assumptions
+@Test.test result[4] == assertions
 
 program_variables = union(argument_variables, body_variables)
 result = parse_function(function_definition)
-@test result.variables == program_variables
-@test result.program == body_wihout_assumptions_and_assertions
-@test result.assumptions == assumptions
-@test result.assertions == assertions
+@Test.test result.variables == program_variables
+@Test.test result.program == body_wihout_assumptions_and_assertions
+@Test.test result.assumptions == assumptions
+@Test.test result.assertions == assertions
 
 
 
@@ -115,105 +115,105 @@ body_wihout_assumptions_and_assertions = body
 
 
 
-@test collect_assumptions(empty_function) == assumptions
-@test parse_arguments(empty_function) == argument_variables
-@test collect_assertions(empty_function) == assertions
-@test collect_unique_variables(empty_function) == body_variables
-@test remove_assertions(body) == body_without_assertions
-@test remove_assumptions(body) == body_without_assumption
+@Test.test collect_assumptions(empty_function) == assumptions
+@Test.test parse_arguments(empty_function) == argument_variables
+@Test.test collect_assertions(empty_function) == assertions
+@Test.test collect_unique_variables(empty_function) == body_variables
+@Test.test remove_assertions(body) == body_without_assertions
+@Test.test remove_assumptions(body) == body_without_assumption
 
 
 result = parse_body(empty_function)
-@test result[1] == body_variables
-@test result[2] == body_wihout_assumptions_and_assertions
-@test result[3] == assumptions
-@test result[4] == assertions
+@Test.test result[1] == body_variables
+@Test.test result[2] == body_wihout_assumptions_and_assertions
+@Test.test result[3] == assumptions
+@Test.test result[4] == assertions
 
 program_variables = union(argument_variables, body_variables)
 result = parse_function(empty_function)
-@test result.variables == program_variables
-@test result.program == body_wihout_assumptions_and_assertions
-@test result.assumptions == assumptions
-@test result.assertions == assertions
+@Test.test result.variables == program_variables
+@Test.test result.program == body_wihout_assumptions_and_assertions
+@Test.test result.assumptions == assumptions
+@Test.test result.assertions == assertions
 
 begin
     formula = :(0.0 <= x)
-    @test formula_to_kyx(formula) == Formula(less_or_equal, nothing, nothing, Expression(real, 0.0, nothing), Expression(symbol, :x, nothing))
+    @Test.test formula_to_kyx(formula) == Formula(less_or_equal, nothing, nothing, Expression(Elenchos.real, 0.0, nothing), Expression(symbol, :x, nothing))
 
     formula = :(0.0 < x)
-    @test formula_to_kyx(formula) == Formula(less, nothing, nothing, Expression(real, 0.0, nothing), Expression(symbol, :x, nothing))
+    @Test.test formula_to_kyx(formula) == Formula(Elenchos.less, nothing, nothing, Expression(Elenchos.real, 0.0, nothing), Expression(symbol, :x, nothing))
 
     formula = :(0.0 >= x)
-    @test formula_to_kyx(formula) == Formula(greater_or_equal, nothing, nothing, Expression(real, 0.0, nothing), Expression(symbol, :x, nothing))
+    @Test.test formula_to_kyx(formula) == Formula(greater_or_equal, nothing, nothing, Expression(Elenchos.real, 0.0, nothing), Expression(symbol, :x, nothing))
 
     formula = :(0.0 > x)
-    @test formula_to_kyx(formula) == Formula(greater, nothing, nothing, Expression(real, 0.0, nothing), Expression(symbol, :x, nothing))
+    @Test.test formula_to_kyx(formula) == Formula(greater, nothing, nothing, Expression(Elenchos.real, 0.0, nothing), Expression(symbol, :x, nothing))
 
     formula = :(0.0 == x)
-    @test formula_to_kyx(formula) == Formula(equal, nothing, nothing, Expression(real, 0.0, nothing), Expression(symbol, :x, nothing))
+    @Test.test formula_to_kyx(formula) == Formula(equal, nothing, nothing, Expression(Elenchos.real, 0.0, nothing), Expression(symbol, :x, nothing))
 
     formula = :(0.0 != x)
-    @test formula_to_kyx(formula) == Formula(not_equal, nothing, nothing, Expression(real, 0.0, nothing), Expression(symbol, :x, nothing))
+    @Test.test formula_to_kyx(formula) == Formula(not_equal, nothing, nothing, Expression(Elenchos.real, 0.0, nothing), Expression(symbol, :x, nothing))
 
     formula = :(0.0 <= x && x <= 1.0)
-    @test formula_to_kyx(formula) == Formula(and, Formula(less_or_equal, nothing, nothing, Expression(real, 0.0, nothing), Expression(symbol, :x, nothing)), Formula(less_or_equal, nothing, nothing, Expression(symbol, :x, nothing), Expression(real, 1.0, nothing)), nothing, nothing)
+    @Test.test formula_to_kyx(formula) == Formula(and, Formula(less_or_equal, nothing, nothing, Expression(Elenchos.real, 0.0, nothing), Expression(symbol, :x, nothing)), Formula(less_or_equal, nothing, nothing, Expression(symbol, :x, nothing), Expression(Elenchos.real, 1.0, nothing)), nothing, nothing)
 
     formula = :(0.0 <= x || x <= 1.0)
-    @test formula_to_kyx(formula) == Formula(or, Formula(less_or_equal, nothing, nothing, Expression(real, 0.0, nothing), Expression(symbol, :x, nothing)), Formula(less_or_equal, nothing, nothing, Expression(symbol, :x, nothing), Expression(real, 1.0, nothing)), nothing, nothing)
+    @Test.test formula_to_kyx(formula) == Formula(or, Formula(less_or_equal, nothing, nothing, Expression(Elenchos.real, 0.0, nothing), Expression(symbol, :x, nothing)), Formula(less_or_equal, nothing, nothing, Expression(symbol, :x, nothing), Expression(Elenchos.real, 1.0, nothing)), nothing, nothing)
 
     formula = :(!(0.0 <= x))
-    @test formula_to_kyx(formula) == Formula(not, Formula(less_or_equal, nothing, nothing, Expression(real, 0.0, nothing), Expression(symbol, :x, nothing)), nothing, nothing, nothing)
+    @Test.test formula_to_kyx(formula) == Formula(not, Formula(less_or_equal, nothing, nothing, Expression(Elenchos.real, 0.0, nothing), Expression(symbol, :x, nothing)), nothing, nothing, nothing)
 end
 
 begin
     expression = :(1.0 + 1.2)
-    @test expression_to_kyx(expression) == Expression(plus, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(plus, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing))
 
     expression = :(1.0 + (1.2 + 1.3)) 
-    @test expression_to_kyx(expression) == Expression(plus, Expression(real, 1.0, nothing), Expression(plus, Expression(real, 1.2, nothing), Expression(real, 1.3, nothing)))
+    @Test.test expression_to_kyx(expression) == Expression(plus, Expression(Elenchos.real, 1.0, nothing), Expression(plus, Expression(Elenchos.real, 1.2, nothing), Expression(Elenchos.real, 1.3, nothing)))
 
     expression = :((1.0 + 1.2) + 1.3)
-    @test expression_to_kyx(expression) == Expression(plus, Expression(plus, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing)), Expression(real, 1.3, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(plus, Expression(plus, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing)), Expression(Elenchos.real, 1.3, nothing))
 
     epression = :(1.0 + 1.2 + 1.3)
-    @test expression_to_kyx(expression) == Expression(plus, Expression(plus, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing)), Expression(real, 1.3, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(plus, Expression(plus, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing)), Expression(Elenchos.real, 1.3, nothing))
 
     expression = :(1.0 - 1.2)
-    @test expression_to_kyx(expression) == Expression(minus, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(minus, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing))
 
     expression = :(1.0 - (1.2 - 1.3))
-    @test expression_to_kyx(expression) == Expression(minus, Expression(real, 1.0, nothing), Expression(minus, Expression(real, 1.2, nothing), Expression(real, 1.3, nothing)))
+    @Test.test expression_to_kyx(expression) == Expression(minus, Expression(Elenchos.real, 1.0, nothing), Expression(minus, Expression(Elenchos.real, 1.2, nothing), Expression(Elenchos.real, 1.3, nothing)))
 
     expression = :((1.0 - 1.2) - 1.3)
-    @test expression_to_kyx(expression) == Expression(minus, Expression(minus, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing)), Expression(real, 1.3, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(minus, Expression(minus, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing)), Expression(Elenchos.real, 1.3, nothing))
 
     expression = :(1.0 * 1.2)
-    @test expression_to_kyx(expression) == Expression(mult, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(mult, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing))
 
     expression = :(1.0 * (1.2 * 1.3))
-    @test expression_to_kyx(expression) == Expression(mult, Expression(real, 1.0, nothing), Expression(mult, Expression(real, 1.2, nothing), Expression(real, 1.3, nothing)))
+    @Test.test expression_to_kyx(expression) == Expression(mult, Expression(Elenchos.real, 1.0, nothing), Expression(mult, Expression(Elenchos.real, 1.2, nothing), Expression(Elenchos.real, 1.3, nothing)))
 
     expression = :((1.0 * 1.2) * 1.3)
-    @test expression_to_kyx(expression) == Expression(mult, Expression(mult, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing)), Expression(real, 1.3, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(mult, Expression(mult, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing)), Expression(Elenchos.real, 1.3, nothing))
 
     expression = :(1.0 * 1.2 * 1.3)
-    @test expression_to_kyx(expression) == Expression(mult, Expression(mult, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing)), Expression(real, 1.3, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(mult, Expression(mult, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing)), Expression(Elenchos.real, 1.3, nothing))
 
     expression = :(1.0 / 1.2)
-    @test expression_to_kyx(expression) == Expression(div, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(Elenchos.div, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing))
 
     expression = :(1.0 / (1.2 / 1.3))
-    @test expression_to_kyx(expression) == Expression(div, Expression(real, 1.0, nothing), Expression(div, Expression(real, 1.2, nothing), Expression(real, 1.3, nothing)))
+    @Test.test expression_to_kyx(expression) == Expression(Elenchos.div, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.div, Expression(Elenchos.real, 1.2, nothing), Expression(Elenchos.real, 1.3, nothing)))
 
     expression = :((1.0 / 1.2) / 1.3)
-    @test expression_to_kyx(expression) == Expression(div, Expression(div, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing)), Expression(real, 1.3, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(Elenchos.div, Expression(Elenchos.div, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing)), Expression(Elenchos.real, 1.3, nothing))
     
     expression = :(1.0 + 1.2 * 1.3 )
-    @test expression_to_kyx(expression) == Expression(plus, Expression(real, 1.0, nothing), Expression(mult, Expression(real, 1.2, nothing), Expression(real, 1.3, nothing)))
+    @Test.test expression_to_kyx(expression) == Expression(plus, Expression(Elenchos.real, 1.0, nothing), Expression(mult, Expression(Elenchos.real, 1.2, nothing), Expression(Elenchos.real, 1.3, nothing)))
 
     expression = :(1.0 * 1.2 + 1.3 )
-    @test expression_to_kyx(expression) == Expression(plus, Expression(mult, Expression(real, 1.0, nothing), Expression(real, 1.2, nothing)), Expression(real, 1.3, nothing))
+    @Test.test expression_to_kyx(expression) == Expression(plus, Expression(mult, Expression(Elenchos.real, 1.0, nothing), Expression(Elenchos.real, 1.2, nothing)), Expression(Elenchos.real, 1.3, nothing))
 
     expression = :(x)
-    @test expression_to_kyx(expression) == Expression(symbol, :x, nothing)
+    @Test.test expression_to_kyx(expression) == Expression(symbol, :x, nothing)
 end
