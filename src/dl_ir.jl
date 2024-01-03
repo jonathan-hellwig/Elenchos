bb906be799ce7a7e043f7d8617e8037a057b78d2
Jonathan Hellwig <jonathan.hellwig@posteo.de>
2024-01-03 11:41:15 +0100

    Move string conversions

--- src/dl_ir.jl
+++ src/dl_ir.jl
@@ -2,15 +2,65 @@
 
 struct Expression
     symbol::ExpressionSymbol
-    left::Union{Expression, Symbol, Real}
-    right::Union{Expression, Nothing}
+    left::Union{Expression,Symbol,Real}
+    right::Union{Expression,Nothing}
 end
-Plus(left::Union{Expression, Symbol, Real}, right::Union{Expression, Symbol, Real}) = Expression(plus, left, right)
-Minus(left::Union{Expression, Symbol, Real}, right::Union{Expression, Symbol, Real}) = Expression(minus, left, right)
-Mult(left::Union{Expression, Symbol, Real}, right::Union{Expression, Symbol, Real}) = Expression(mult, left, right)
-Div(left::Union{Expression, Symbol, Real}, right::Union{Expression, Symbol, Real}) = Expression(div, left, right)
+Plus(left::Union{Expression,Symbol,Real}, right::Union{Expression,Symbol,Real}) = Expression(plus, left, right)
+Minus(left::Union{Expression,Symbol,Real}, right::Union{Expression,Symbol,Real}) = Expression(minus, left, right)
+Mult(left::Union{Expression,Symbol,Real}, right::Union{Expression,Symbol,Real}) = Expression(mult, left, right)
+Div(left::Union{Expression,Symbol,Real}, right::Union{Expression,Symbol,Real}) = Expression(div, left, right)
 DlReal(value::Real) = Expression(real, value, nothing)
 DlSymbol(dlsymbol::Symbol) = Expression(symbol, dlsymbol, nothing)
+
+@enum FormulaSymbol less_or_equal greater_or_equal less greater equal not_equal and or not bool_true bool_false
+"""
+    Formula
+    Syntax: e, q, E, Q := e <= q, e < q, e = q, e > q, e >= q, e != q, E && Q, E || Q, !E
+"""
+struct Formula
+    symbol::FormulaSymbol
+    # Has either zero, one or two subformulas
+    first_subformula::Union{Formula,Nothing}
+    second_subformula::Union{Formula,Nothing}
+    # Has either zero, one or two subexpressions
+    first_expressions::Union{Expression,Nothing}
+    second_expressions::Union{Expression,Nothing}
+end
+
+Not(formula::Formula) = Formula(not, formula, nothing, nothing, nothing)
+And(formula1::Formula, formula2::Formula) = Formula(and, formula1, formula2, nothing, nothing)
+Or(formula1::Formula, formula2::Formula) = Formula(or, formula1, formula2, nothing, nothing)
+LessOrEqual(expression1::Expression, expression2::Expression) = Formula(less_or_equal, nothing, nothing, expression1, expression2)
+GreaterOrEqual(expression1::Expression, expression2::Expression) = Formula(greater_or_equal, nothing, nothing, expression1, expression2)
+Less(expression1::Expression, expression2::Expression) = Formula(less, nothing, nothing, expression1, expression2)
+Greater(expression1::Expression, expression2::Expression) = Formula(greater, nothing, nothing, expression1, expression2)
+Equal(expression1::Expression, expression2::Expression) = Formula(equal, nothing, nothing, expression1, expression2)
+NotEqual(expression1::Expression, expression2::Expression) = Formula(not_equal, nothing, nothing, expression1, expression2)
+BoolTrue() = Formula(bool_true, nothing, nothing, nothing, nothing)
+BoolFalse() = Formula(bool_false, nothing, nothing, nothing, nothing)
+
+@enum ProgramSymbol assign choice sequential dl_test empty
+
+"""
+    Program
+    A program is a sequence of assignments, choices and tests.
+    Syntax: α, β, Q, x, e ::= α;β | ?Q | α ∪ β | x := e
+"""
+struct Program
+    #TODO: Introduce partial constructors
+    symbol::ProgramSymbol
+    first_programs::Union{Program,Nothing}
+    second_programs::Union{Program,Nothing}
+    formula::Union{Formula,Nothing}
+    expressions::Union{Tuple{Expression,Expression},Nothing}
+end
+#TODO: Add a custom constructor for each symbol
+Assignment(symbol::Expression, expression::Expression) = Program(assign, nothing, nothing, nothing, (symbol, expression))
+Choice(first_program::Program, second_program::Program) = Program(choice, first_program, second_program, nothing, nothing)
+Sequential(first_program::Union{Program,Nothing}, second_program::Union{Program,Nothing}) = Program(sequential, first_program, second_program, nothing, nothing)
+Empty() = Program(empty, nothing, nothing, nothing, nothing)
+DlTest(formula::Formula) = Program(dl_test, nothing, nothing, formula, nothing)
+
 function expression_to_kyx(expression)
     if isa(expression, Real)
         return Expression(real, expression, nothing)
@@ -42,33 +92,6 @@ function expression_to_kyx(expression)
     return kyx_expression
 end
 
-@enum FormulaSymbol less_or_equal greater_or_equal less greater equal not_equal and or not bool_true bool_false
-"""
-    Formula
-    Syntax: e, q, E, Q := e <= q, e < q, e = q, e > q, e >= q, e != q, E && Q, E || Q, !E
-"""
-struct Formula
-    symbol::FormulaSymbol
-    # Has either zero, one or two subformulas
-    first_subformula::Union{Formula, Nothing}
-    second_subformula::Union{Formula, Nothing}
-    # Has either zero, one or two subexpressions
-    first_expressions::Union{Expression, Nothing}
-    second_expressions::Union{Expression, Nothing}
-end
-
-Not(formula::Formula) = Formula(not, formula, nothing, nothing, nothing)
-And(formula1::Formula, formula2::Formula) = Formula(and, formula1, formula2, nothing, nothing)
-Or(formula1::Formula, formula2::Formula) = Formula(or, formula1, formula2, nothing, nothing)
-LessOrEqual(expression1::Expression, expression2::Expression) = Formula(less_or_equal, nothing, nothing, expression1, expression2)
-GreaterOrEqual(expression1::Expression, expression2::Expression) = Formula(greater_or_equal, nothing, nothing, expression1, expression2)
-Less(expression1::Expression, expression2::Expression) = Formula(less, nothing, nothing, expression1, expression2)
-Greater(expression1::Expression, expression2::Expression) = Formula(greater, nothing, nothing, expression1, expression2)
-Equal(expression1::Expression, expression2::Expression) = Formula(equal, nothing, nothing, expression1, expression2)
-NotEqual(expression1::Expression, expression2::Expression) = Formula(not_equal, nothing, nothing, expression1, expression2)
-BoolTrue() = Formula(bool_true, nothing, nothing, nothing, nothing)
-BoolFalse() = Formula(bool_false, nothing, nothing, nothing, nothing)
-
 #TODO: Allow for the usage of true and false
 function formula_to_kyx(formula)
     symbol_to_formula = Dict(
@@ -93,7 +116,7 @@ function formula_to_kyx(formula)
         kyx_formula = Formula(symbol_to_formula[formula.head], formula_to_kyx(formula.args[1]), formula_to_kyx(formula.args[2]), nothing, nothing)
     elseif formula.head == :call
         symbol = formula.args[1]
-        
+
         if symbol in [:<=, :>=, :<, :>, :(==), :!=]
             kyx_formula = Formula(symbol_to_formula[symbol], nothing, nothing, expression_to_kyx(formula.args[2]), expression_to_kyx(formula.args[3]))
         elseif symbol == :!
@@ -103,29 +126,6 @@ function formula_to_kyx(formula)
     return kyx_formula
 
 end
-
-
-@enum ProgramSymbol assign choice sequential dl_test empty
-
-"""
-    Program
-    A program is a sequence of assignments, choices and tests.
-    Syntax: α, β, Q, x, e ::= α;β | ?Q | α ∪ β | x := e
-"""
-struct Program
-    #TODO: Introduce partial constructors
-    symbol::ProgramSymbol
-    first_programs::Union{Program, Nothing}
-    second_programs::Union{Program, Nothing}
-    formula::Union{Formula, Nothing}
-    expressions::Union{Tuple{Expression, Expression}, Nothing}
-end
-#TODO: Add a custom constructor for each symbol
-Assignment(symbol::Expression, expression::Expression) = Program(assign, nothing, nothing, nothing, (symbol, expression))
-Choice(first_program::Program, second_program::Program) = Program(choice, first_program, second_program, nothing, nothing)
-Sequential(first_program::Union{Program, Nothing}, second_program::Union{Program, Nothing}) = Program(sequential, first_program, second_program, nothing, nothing)
-Empty() = Program(empty, nothing, nothing, nothing, nothing)
-DlTest(formula::Formula) = Program(dl_test, nothing, nothing, formula, nothing)
 
 # TODO: Add the empty program
 function program_to_kyx(program)
@@ -157,7 +157,68 @@ function program_to_kyx(program)
     return kyx_program
 end
 
+function expression_to_string(expression::Expression)
+    symbol_to_string = Dict(
+        plus => "+",
+        minus => "-",
+        mult => "*",
+        Elenchos.div => "/"
+    )
+    if expression.symbol == symbol
+        return "$(expression.left)"
+    elseif expression.symbol == Elenchos.real
+        return "$(expression.left)"
+    elseif expression.symbol in [plus, minus, mult, Elenchos.div]
+        return "$(expression_to_string(expression.left)) " * symbol_to_string[expression.symbol] * " $(expression_to_string(expression.right))"
+    end
+end
 
+
+function formula_to_string(formula::Formula)
+    symbol_to_string = Dict(
+        less_or_equal => "<=",
+        greater_or_equal => ">=",
+        Elenchos.less => "<",
+        greater => ">",
+        equal => "=",
+        not_equal => "!=",
+        and => "&",
+        or => "|",
+        not => "!",
+        bool_true => "true",
+        bool_false => "false"
+    )
+    if formula.symbol in [less_or_equal, greater_or_equal, Elenchos.less, greater, equal, not_equal]
+        return "$(expression_to_string(formula.first_expressions)) " * symbol_to_string[formula.symbol] * " $(expression_to_string(formula.second_expressions))"
+    elseif formula.symbol in [and, or]
+        return "$(formula_to_string(formula.first_subformula)) " * symbol_to_string[formula.symbol] * " $(formula_to_string(formula.second_subformula))"
+    elseif formula.symbol == not
+        return symbol_to_string[formula.symbol] * "($(formula_to_string(formula.first_subformula)))"
+    elseif formula.symbol in [bool_true, bool_false]
+        return symbol_to_string[formula.symbol]
+    end
+end
+
+
+function program_to_string(program::Program)
+    symbol_to_string = Dict(
+        assign => ":=",
+        choice => "∪",
+        sequential => ";",
+        dl_test => "?"
+    )
+    if program.symbol == assign
+        return "$(expression_to_string(program.expressions[1])) " * symbol_to_string[program.symbol] * " $(expression_to_string(program.expressions[2]))"
+    elseif program.symbol == choice
+        return "$(program_to_string(program.first_programs)) " * symbol_to_string[program.symbol] * " $(program_to_string(program.second_programs))"
+    elseif program.symbol == sequential
+        return "$(program_to_string(program.first_programs))" * symbol_to_string[program.symbol] * " $(program_to_string(program.second_programs))"
+    elseif program.symbol == dl_test
+        return symbol_to_string[program.symbol] * "($(formula_to_string(program.formula)))"
+    end
+end
+
+export expression_to_string, formula_to_string, program_to_string
 export ExpressionSymbol, plus, minus, mult, div, real, symbol
 export FormulaSymbol, less_or_equal, greater_or_equal, less, greater, equal, not_equal, and, or, not, bool_true, bool_false
 export expression_to_kyx, formula_to_kyx, Expression, Formula
--- test/string.jl
+++ test/string.jl
@@ -2,21 +2,7 @@ import Test
 using Elenchos
 
 
-function expression_to_string(expression::Expression)
-    symbol_to_string = Dict(
-        plus => "+",
-        minus => "-",
-        mult => "*",
-        Elenchos.div => "/"
-    )
-    if expression.symbol == symbol
-        return "$(expression.left)"
-    elseif expression.symbol == Elenchos.real
-        return "$(expression.left)"
-    elseif expression.symbol in [plus, minus, mult, Elenchos.div]
-        return "$(expression_to_string(expression.left)) " * symbol_to_string[expression.symbol] * " $(expression_to_string(expression.right))"
-    end
-end
+
 
 @Test.test expression_to_string(DlSymbol(:x)) == "x"
 
@@ -30,30 +16,6 @@ end
 
 @Test.test expression_to_string(Div(DlSymbol(:x), DlReal(1.0))) == "x / 1.0"
 
-function formula_to_string(formula::Formula)
-    symbol_to_string = Dict(
-        less_or_equal => "<=",
-        greater_or_equal => ">=",
-        Elenchos.less => "<",
-        greater => ">",
-        equal => "=",
-        not_equal => "!=",
-        and => "&",
-        or => "|",
-        not => "!",
-        bool_true => "true",
-        bool_false => "false"
-    )
-    if formula.symbol in [less_or_equal, greater_or_equal, Elenchos.less, greater, equal, not_equal]
-        return "$(expression_to_string(formula.first_expressions)) " * symbol_to_string[formula.symbol] * " $(expression_to_string(formula.second_expressions))"
-    elseif formula.symbol in [and, or]
-        return "$(formula_to_string(formula.first_subformula)) " * symbol_to_string[formula.symbol] * " $(formula_to_string(formula.second_subformula))"
-    elseif formula.symbol == not
-        return symbol_to_string[formula.symbol] * "($(formula_to_string(formula.first_subformula)))"
-    elseif formula.symbol in [bool_true, bool_false]
-        return symbol_to_string[formula.symbol]
-    end
-end
 
 @Test.test formula_to_string(BoolTrue()) == "true"
 @Test.test formula_to_string(BoolFalse()) == "false"
@@ -65,24 +27,6 @@ end
 @Test.test formula_to_string(Or(Greater(DlReal(3), DlReal(2)), Equal(DlReal(4),DlReal(4)))) == "3 > 2 | 4 = 4"
 @Test.test formula_to_string(Not(Or(Greater(DlReal(3), DlReal(2)), Equal(DlReal(4),DlReal(4))))) == "!(3 > 2 | 4 = 4)"
 
-
-function program_to_string(program::Program)
-    symbol_to_string = Dict(
-        assign => ":=",
-        choice => "∪",
-        sequential => ";",
-        dl_test => "?"
-    )
-    if program.symbol == assign
-        return "$(expression_to_string(program.expressions[1])) " * symbol_to_string[program.symbol] * " $(expression_to_string(program.expressions[2]))"
-    elseif program.symbol == choice
-        return "$(program_to_string(program.first_programs)) " * symbol_to_string[program.symbol] * " $(program_to_string(program.second_programs))"
-    elseif program.symbol == sequential
-        return "$(program_to_string(program.first_programs))" * symbol_to_string[program.symbol] * " $(program_to_string(program.second_programs))"
-    elseif program.symbol == dl_test
-        return symbol_to_string[program.symbol] * "($(formula_to_string(program.formula)))"
-    end
-end
 
 @Test.test program_to_string(Assignment(DlSymbol(:x), DlReal(1.0))) == "x := 1.0"
 @Test.test program_to_string(Sequential(Assignment(DlSymbol(:x), DlReal(1.0)), Assignment(DlSymbol(:y), DlReal(2.0)))) == "x := 1.0; y := 2.0"
