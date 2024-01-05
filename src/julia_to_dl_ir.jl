3dcdadfbc9e3c5b40a6ad29bcea46465a31df898
Jonathan Hellwig <jonathan.hellwig@posteo.de>
2024-01-05 10:59:45 +0100

    Rename conversion functions

--- src/julia_to_dl_ir.jl
+++ src/julia_to_dl_ir.jl
@@ -61,7 +61,7 @@ Sequential(first_program::Union{Program,Nothing}, second_program::Union{Program,
 Empty() = Program(empty, nothing, nothing, nothing, nothing)
 DlTest(formula::Formula) = Program(dl_test, nothing, nothing, formula, nothing)
 
-function expression_to_kyx(expression)
+function expression_to_dl_ir(expression)
     if isa(expression, Real)
         return Expression(real, expression, nothing)
     elseif isa(expression, Symbol)
@@ -73,19 +73,19 @@ function expression_to_kyx(expression)
 
     expression_symbol = expression.args[1]
     if expression_symbol == :+
-        kyx_expression = Expression(plus, expression_to_kyx(expression.args[2]), expression_to_kyx(expression.args[3]))
+        kyx_expression = Expression(plus, expression_to_dl_ir(expression.args[2]), expression_to_dl_ir(expression.args[3]))
         for i in 4:length(expression.args)
-            kyx_expression = Expression(plus, kyx_expression, expression_to_kyx(expression.args[i]))
+            kyx_expression = Expression(plus, kyx_expression, expression_to_dl_ir(expression.args[i]))
         end
     elseif expression_symbol == :-
-        kyx_expression = Expression(minus, expression_to_kyx(expression.args[2]), expression_to_kyx(expression.args[3]))
+        kyx_expression = Expression(minus, expression_to_dl_ir(expression.args[2]), expression_to_dl_ir(expression.args[3]))
     elseif expression_symbol == :*
-        kyx_expression = Expression(mult, expression_to_kyx(expression.args[2]), expression_to_kyx(expression.args[3]))
+        kyx_expression = Expression(mult, expression_to_dl_ir(expression.args[2]), expression_to_dl_ir(expression.args[3]))
         for i in 4:length(expression.args)
-            kyx_expression = Expression(mult, kyx_expression, expression_to_kyx(expression.args[i]))
+            kyx_expression = Expression(mult, kyx_expression, expression_to_dl_ir(expression.args[i]))
         end
     elseif expression_symbol == :/
-        kyx_expression = Expression(div, expression_to_kyx(expression.args[2]), expression_to_kyx(expression.args[3]))
+        kyx_expression = Expression(div, expression_to_dl_ir(expression.args[2]), expression_to_dl_ir(expression.args[3]))
     else
         error("Unknown operator!")
     end
@@ -93,7 +93,7 @@ function expression_to_kyx(expression)
 end
 
 #TODO: Allow for the usage of true and false
-function formula_to_kyx(formula)
+function formula_to_dl_ir(formula)
     symbol_to_formula = Dict(
         :<= => less_or_equal,
         :>= => greater_or_equal,
@@ -113,14 +113,14 @@ function formula_to_kyx(formula)
         end
         kyx_formula = Formula(value, nothing, nothing, nothing, nothing)
     elseif formula.head in [:&&, :||]
-        kyx_formula = Formula(symbol_to_formula[formula.head], formula_to_kyx(formula.args[1]), formula_to_kyx(formula.args[2]), nothing, nothing)
+        kyx_formula = Formula(symbol_to_formula[formula.head], formula_to_dl_ir(formula.args[1]), formula_to_dl_ir(formula.args[2]), nothing, nothing)
     elseif formula.head == :call
         symbol = formula.args[1]
 
         if symbol in [:<=, :>=, :<, :>, :(==), :!=]
-            kyx_formula = Formula(symbol_to_formula[symbol], nothing, nothing, expression_to_kyx(formula.args[2]), expression_to_kyx(formula.args[3]))
+            kyx_formula = Formula(symbol_to_formula[symbol], nothing, nothing, expression_to_dl_ir(formula.args[2]), expression_to_dl_ir(formula.args[3]))
         elseif symbol == :!
-            kyx_formula = Formula(symbol_to_formula[symbol], formula_to_kyx(formula.args[2]), nothing, nothing, nothing)
+            kyx_formula = Formula(symbol_to_formula[symbol], formula_to_dl_ir(formula.args[2]), nothing, nothing, nothing)
         end
     end
     return kyx_formula
@@ -128,40 +128,40 @@ function formula_to_kyx(formula)
 end
 
 # TODO: Add error handling for unsupported programs, e.g. if there are more than 2 branches in a choice, or if the program uses return
-function program_to_kyx(program)
+function program_to_dl_ir(program)
     if program.head == :block
         if length(program.args) == 0
             kyx_program = Empty()
         else
             new_program = Expr(program.head, program.args[2:end]...)
-            kyx_program = Sequential(program_to_kyx(program.args[1]), program_to_kyx(new_program))
+            kyx_program = Sequential(program_to_dl_ir(program.args[1]), program_to_dl_ir(new_program))
         end
     elseif program.head == :if
         # TODO: Handle the case where there is no else branch and when there are more than 2 branches
-        if_formula = formula_to_kyx(program.args[1])
+        if_formula = formula_to_dl_ir(program.args[1])
         if_condition = DlTest(if_formula)
-        if_block = program_to_kyx(program.args[2])
+        if_block = program_to_dl_ir(program.args[2])
         else_formula = Formula(Elenchos.not, if_formula, nothing, nothing, nothing)
         else_condition = DlTest(else_formula)
         if length(program.args) == 2
             else_block = Empty()
         elseif length(program.args) == 3
-            else_block = program_to_kyx(program.args[3])
+            else_block = program_to_dl_ir(program.args[3])
         end
         first_choice = Sequential(if_condition, if_block)
         second_choice = Sequential(else_condition, else_block)
         kyx_program = Choice(first_choice, second_choice)
     elseif program.head == :(=)
-        kyx_program = Assignment(expression_to_kyx(program.args[1]), expression_to_kyx(program.args[2]))
+        kyx_program = Assignment(expression_to_dl_ir(program.args[1]), expression_to_dl_ir(program.args[2]))
     end
     return kyx_program
 end
 
 export ExpressionSymbol, plus, minus, mult, div, real, symbol
 export FormulaSymbol, less_or_equal, greater_or_equal, less, greater, equal, not_equal, and, or, not, bool_true, bool_false
-export expression_to_kyx, formula_to_kyx, Expression, Formula
+export expression_to_dl_ir, formula_to_dl_ir, Expression, Formula
 export Not, And, Or, LessOrEqual, GreaterOrEqual, Less, Greater, Equal, NotEqual, BoolTrue, BoolFalse
 export ProgramSymbol, assign, choice, sequential, dl_test
 export Program, Assignment, Choice, Sequential, Empty, DlTest
-export program_to_kyx
+export program_to_dl_ir
 export DlReal, DlSymbol, Plus, Minus, Mult, Div
