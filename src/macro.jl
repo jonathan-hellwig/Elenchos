487c3824a6286f6a65a951164979e139b2a3d9f9
Jonathan Hellwig <jonathan.hellwig@posteo.de>
2024-01-09 11:38:50 +0100

    Implement iterative proofing

--- src/macro.jl
+++ src/macro.jl
@@ -3,49 +3,28 @@ export @elenchos
 using MacroTools
 
 function parse_function(ex)
-    @capture(ex, (function f_(xs__) body_ end) | (f_(xs__) = body_))
+    ex = MacroTools.prewalk(rmlines, ex)
+    @capture(ex, (function f_(xs__)
+        body_
+    end) | (f_(xs__) = body_))
     argument_variables = parse_arguments(xs)
-    body_variables, program, assumptions, assertions = parse_body(body)
+    body_variables, programs, assertions = parse_body(body)
     variables = union(argument_variables, body_variables)
-    return variables, program, assumptions, assertions
+    return variables, programs, assertions
 end
 
 function parse_body(function_body::Expr)
     function_body = MacroTools.prewalk(rmlines, function_body)
     assertions = collect_assertions(function_body)
-    assumptions = collect_assumptions(function_body)
     variables = collect_unique_variables(function_body)
+    programs = collect_programs(function_body)
 
-    program = remove_assertions(function_body)
-    program = remove_assumptions(program)
-
-    return variables, program, assumptions, assertions
-end
-
-
-function collect_assumptions(function_body)
-    # TODO: Make sure that the assumptions are stated in the first line of the function
-    # TODO: Make this work if assumptions are nested in if statements or loops
-    assumptions = Vector{Expr}()
-    for x in function_body.args
-        if isa(x, Expr) && @capture(x, @assume q_)
-            push!(assumptions, q)
-        end
-    end
-    return assumptions
-end
-
-function remove_assertions(body::Expr)
-    return Expr(body.head, filter(x->!@capture(x, @assert q_), body.args)...)
-end
-
-function remove_assumptions(body::Expr)
-    return Expr(body.head, filter(x->!@capture(x, @assume q_), body.args)...)
+    return variables, programs, assertions
 end
 
 function collect_unique_variables(body::Expr)
     queue = [body]
-    variables = Set{Tuple{Symbol, Symbol}}()
+    variables = Set{Tuple{Symbol,Symbol}}()
     while !isempty(queue)
         x = pop!(queue)
         #TODO: Check whether you need type inference
@@ -61,7 +40,7 @@ end
 
 function parse_arguments(arguments)
     #TODO: Check whether this is robust
-    variables = Set{Tuple{Symbol, Symbol}}()
+    variables = Set{Tuple{Symbol,Symbol}}()
     for x in arguments
         @capture(x, y_::t_)
         push!(variables, (y, t))
@@ -70,45 +49,93 @@ function parse_arguments(arguments)
 end
 
 function collect_assertions(function_body)
-    # TODO: Make sure that the assertions are stated in the first line of the function
+    if isempty(function_body.args)
+        return [[:(true)], [:(true)]]
+    end
     # This does not work if assertions are nested in if statements
-    last_is_true = false
-    assertions = [[]]
+    program_not_empty = false
+    last_is_assert = false
+    assertions = []
+
     for x in function_body.args
-        if isa(x, Expr) && @capture(x, @assert q_) && !last_is_true
-            push!(assertions, [q])
-            last_is_true = true
-        elseif isa(x, Expr) && @capture(x, @assert q_) && last_is_true
+        is_assert = isa(x, Expr) && @capture(x, @assert q_)
+        if is_assert && !last_is_assert
+            push!(assertions, Vector{Union{Expr,Bool}}([q]))
+            last_is_assert = true
+        elseif is_assert && last_is_assert
             push!(assertions[end], q)
         else
-            last_is_true = false
+            last_is_assert = false
+            program_not_empty = true
+        end
+    end
+    # TODO: Fix the case where the program is empty
+    is_assert = isa(function_body.args[1], Expr) && @capture(function_body.args[1], @assert q_)
+    if is_assert && last_is_assert && !program_not_empty
+        pushfirst!(assertions, [:(true)])
+    else
+        if !last_is_assert
+            push!(assertions, [:(true)])
+        end
+        if !is_assert
+            pushfirst!(assertions, [:(true)])
         end
     end
     return assertions
 end
 
-function collect_programs(function_body) 
-    last_is_true = false
+function collect_lines(function_body)
     programs = [[]]
+    has_assertions = false
+    last_is_assert = false
     for x in function_body.args
-        if isa(x, Expr) && @capture(x, @assert q_) && !last_is_true
-            push!(programs, [])
-            last_is_true = true
-        elseif !(isa(x, Expr) && @capture(x, @assert q_))
-            push!(programs[end], x)
-            last_is_true = false
+        if isa(x, Expr) && @capture(x, @assert q_)
+            last_is_assert = true
+        else
+            if has_assertions && last_is_assert
+                push!(programs, [x])
+            else
+                has_assertions = true
+                push!(programs[end], x)
+            end
+            last_is_assert = false
         end
+    end
+
+    return programs
+end
+
+function collect_programs(function_body)
+    programs = []
+    for program in collect_lines(function_body)
+        push!(programs, Expr(:block, program...))
     end
     return programs
 end
 
+function propagate_formulas(formulas, programs)
+    formulas = deepcopy(formulas)
+    for (current_formulas, next_formulas, program) in zip(formulas[1:end-1], formulas[2:end], programs)
+        modified_variables = get_modified_variables(program)
+        for formula in current_formulas
+            free_variables = get_variables(formula)
+            if isempty(intersect(free_variables, modified_variables))
+                push!(next_formulas, formula)
+            end
+        end
+    end
+    return formulas
+end
 
 include("to_kyx_string.jl")
 
 macro elenchos(function_definition)
-    variables, program, assumptions, assertions = parse_function(function_definition)
-    program_ir = program_to_dl_ir(program)
-    assumptions_ir = map(x -> formula_to_dl_ir(x), assumptions)
-    assertions_ir = map(x -> formula_to_dl_ir(x), assertions)
-    return to_kyx_file_string(variables, assumptions_ir, assertions_ir, program_ir)
+    variables, programs, assertions = parse_function(function_definition)
+    programs_ir = map(x -> program_to_dl_ir(x), programs)
+    assertions_ir = [map(x -> formula_to_dl_ir(x), assertion) for assertion in assertions]
+    propagated_assertions_ir = propagate_formulas(assertions_ir, programs_ir)
+
+    for (program, assumptions, assertions) in zip(programs_ir, propagated_assertions_ir[1:end-1], propagated_assertions_ir[2:end])
+        println(to_kyx_file_string(variables, assumptions, assertions, program))
+    end
 end
--- test/test_macro.jl
+++ test/test_macro.jl
@@ -1,11 +1,11 @@
 import Test
 using MacroTools
-using Elenchos: remove_assertions, remove_assumptions, collect_assumptions, parse_arguments, collect_assertions, collect_programs, collect_unique_variables, parse_body, parse_function
+using Elenchos: parse_arguments, collect_assertions, collect_programs, collect_unique_variables, parse_body, parse_function, collect_lines, propagate_formulas
 import Elenchos
 Test.@testset "Test macro" begin
     function_definition = quote
         function max(x::Real, y::Real)
-            @assume 0 <= x && 0 <= y
+            @assert 0 <= x && 0 <= y
             if x >= y
                 max_value = x
             else
@@ -14,13 +14,12 @@ Test.@testset "Test macro" begin
 
             @assert max_value >= x && max_value >= y
             @assert max_value == x || max_value == y
-            return max_value
         end
     end
     function_definition = MacroTools.prewalk(rmlines, function_definition)
     arguments = [:(x::Real), :(y::Real)]
     body = quote
-        @assume 0 <= x && 0 <= y
+        @assert 0 <= x && 0 <= y
         if x >= y
             max_value = x
         else
@@ -29,74 +28,17 @@ Test.@testset "Test macro" begin
 
         @assert max_value >= x && max_value >= y
         @assert max_value == x || max_value == y
-        return max_value
     end
 
     body = MacroTools.prewalk(rmlines, body)
 
-    body_without_assertions = quote
-        @assume 0 <= x && 0 <= y
-        if x >= y
-            max_value = x
-        else
-            max_value = y
-        end
-        return max_value
-    end
-    body_without_assertions = MacroTools.prewalk(rmlines, body_without_assertions)
-
-    body_without_assumption = quote
-        if x >= y
-            max_value = x
-        else
-            max_value = y
-        end
-
-        @assert max_value >= x && max_value >= y
-        @assert max_value == x || max_value == y
-        return max_value
-    end
-    body_without_assumption = MacroTools.prewalk(rmlines, body_without_assumption)
-
-    body_wihout_assumptions_and_assertions = quote
-        if x >= y
-            max_value = x
-        else
-            max_value = y
-        end
-        return max_value
-    end
-    body_wihout_assumptions_and_assertions = MacroTools.prewalk(rmlines, body_wihout_assumptions_and_assertions)
-
-
-    assumptions = [:(0 <= x && 0 <= y)]
-    assertions = [[], [:(max_value >= x && max_value >= y), :(max_value == x || max_value == y)]]
+    assertions = [[:(0 <= x && 0 <= y)], [:(max_value >= x && max_value >= y), :(max_value == x || max_value == y)]]
     body_variables = Set{Tuple{Symbol,Symbol}}([(:max_value, :Real)])
     argument_variables = Set{Tuple{Symbol,Symbol}}([(:x, :Real), (:y, :Real)])
 
-
-
-    Test.@test collect_assumptions(body) == assumptions
     Test.@test parse_arguments(arguments) == argument_variables
     Test.@test collect_assertions(body) == assertions
-    Test.@test remove_assertions(body) == body_without_assertions
-    Test.@test remove_assumptions(body) == body_without_assumption
     Test.@test collect_unique_variables(body) == body_variables
-
-
-    result = parse_body(body)
-    Test.@test result[1] == body_variables
-    Test.@test result[2] == body_wihout_assumptions_and_assertions
-    Test.@test result[3] == assumptions
-    Test.@test result[4] == assertions
-
-    program_variables = union(argument_variables, body_variables)
-    result = parse_function(function_definition)
-    Test.@test result[1] == program_variables
-    Test.@test result[2] == body_wihout_assumptions_and_assertions
-    Test.@test result[3] == assumptions
-    Test.@test result[4] == assertions
-
 
 
     empty_function = quote
@@ -109,72 +51,171 @@ Test.@testset "Test macro" begin
 
     assumptions = []
     argument_variables = Set{Tuple{Symbol,Symbol}}()
-    assertions = [[]]
+    assertions = [[:(true)], [:(true)]]
     body_variables = Set{Tuple{Symbol,Symbol}}()
-    body_without_assertions = body
-    body_without_assumption = body
-    body_wihout_assumptions_and_assertions = body
 
-
-
-    Test.@test collect_assumptions(body) == assumptions
     Test.@test parse_arguments(arguments) == argument_variables
     Test.@test collect_assertions(body) == assertions
     Test.@test collect_unique_variables(body) == body_variables
-    Test.@test remove_assertions(body) == body_without_assertions
-    Test.@test remove_assumptions(body) == body_without_assumption
 
+    function_definition = Base.remove_linenums!(quote
+        function max(x::Real, y::Real)
+            @assert 0 <= x && 0 <= y
+            if x >= y
+                max_value = x
+            else
+                max_value = y
+            end
+            @assert max_value >= x && max_value >= y
+            @assert max_value == x || max_value == y
+        end
+    end)
 
-    result = parse_body(body)
-    Test.@test result[1] == body_variables
-    Test.@test result[2] == body_wihout_assumptions_and_assertions
-    Test.@test result[3] == assumptions
-    Test.@test result[4] == assertions
+    variables, programs, assertions = parse_function(function_definition)
 
-    program_variables = union(argument_variables, body_variables)
-    result = parse_function(empty_function)
-    Test.@test result[1] == program_variables
-    Test.@test result[2] == body_wihout_assumptions_and_assertions
-    Test.@test result[3] == assumptions
-    Test.@test result[4] == assertions
+    Test.@test variables == Set{Tuple{Symbol,Symbol}}([(:max_value, :Real), (:x, :Real), (:y, :Real)])
+    # TODO: programs consists of two elements, but it should be one
+    Test.@test programs == [Base.remove_linenums!(quote
+        if x >= y
+            max_value = x
+        else
+            max_value = y
+        end
+    end)]
+    Test.@test assertions == [[:(0 <= x && 0 <= y)], [:(max_value >= x && max_value >= y), :(max_value == x || max_value == y)]]
 end
 
 Test.@testset "Test collect_assertions and collect_programs" begin
     function_body = Base.remove_linenums!(quote
-            @assert true
-            @assert x == 2
-            x = 1
-            @assert x == 2
-            @assert x == 1
+        @assert true
+        @assert x == 2
+        x = 1
+        @assert x == 2
+        @assert x == 1
     end)
 
-    Test.@test collect_assertions(function_body) == [[], [true, :(x == 2)], [:(x == 2), :(x == 1)]]
-    Test.@test collect_programs(function_body) == [[], [:(x = 1)], []]
+    Test.@test collect_assertions(function_body) == [[true, :(x == 2)], [:(x == 2), :(x == 1)]]
+    Test.@test collect_lines(function_body) == [[:(x = 1)]]
 
     function_body = Base.remove_linenums!(quote
-            @assert true
-            x = 1
-            @assert x == 2
+        @assert true
+        x = 1
+        @assert x == 2
     end)
 
-    Test.@test collect_assertions(function_body) == [[], [true], [:(x == 2)]]
-    Test.@test collect_programs(function_body) == [[], [:(x = 1)], []]
+    Test.@test collect_assertions(function_body) == [[:(true)], [:(x == 2)]]
+    Test.@test collect_lines(function_body) == [[:(x = 1)]]
 
     function_body = Base.remove_linenums!(quote
-            @assert true
-            @assert x == 2
-            @assert x == 1
+        @assert true
+        @assert x == 2
+        @assert x == 1
     end)
 
-    Test.@test collect_assertions(function_body) == [[], [true, :(x == 2), :(x == 1)]]
-    Test.@test collect_programs(function_body) == [[], []]
+    Test.@test collect_assertions(function_body) == [[:(true)], [:(true), :(x == 2), :(x == 1)]]
+    Test.@test collect_lines(function_body) == [[]]
 
     function_body = Base.remove_linenums!(quote
-            x = 1
-            x = 2
-            x = 3
+        x = 1
+        x = 2
+        x = 3
     end)
 
-    Test.@test collect_assertions(function_body) == [[]]
-    Test.@test collect_programs(function_body) == [[:(x = 1), :(x = 2), :(x = 3)]]
+    Test.@test collect_assertions(function_body) == [[:(true)], [:(true)]]
+    Test.@test collect_lines(function_body) == [[:(x = 1), :(x = 2), :(x = 3)]]
+
+    function_body = Base.remove_linenums!(quote
+        @assert y == 1
+        x = 1
+    end)
+    Test.@test collect_assertions(function_body) == [[:(y == 1)], [:(true)]]
+    Test.@test collect_lines(function_body) == [[:(x = 1)]]
+
+    function_body = Base.remove_linenums!(quote
+        x = 1
+        @assert y == 1
+    end)
+    Test.@test collect_assertions(function_body) == [[:(true)], [:(y == 1)]]
+    Test.@test collect_lines(function_body) == [[:(x = 1)]]
+
+
+    function_body = Base.remove_linenums!(quote
+        x = 1
+    end)
+    Test.@test collect_assertions(function_body) == [[:(true)], [:(true)]]
+    Test.@test collect_lines(function_body) == [[:(x = 1)]]
+
+    function_body = Base.remove_linenums!(quote
+        @assert y == 1
+        x = 1
+        @assert y == 1
+    end)
+    Test.@test collect_assertions(function_body) == [[:(y == 1)], [:(y == 1)]]
+    Test.@test collect_lines(function_body) == [[:(x = 1)]]
+
+    function_body = Base.remove_linenums!(quote
+        @assert y == 1
+    end)
+    Test.@test collect_assertions(function_body) == [[:(true)], [:(y == 1)]]
+    Test.@test collect_lines(function_body) == [[]]
+
+    function_body = Base.remove_linenums!(quote
+        @assert y == 1
+        @assert x == 1
+    end)
+
+    Test.@test collect_assertions(function_body) == [[:(true)], [:(y == 1), :(x == 1)]]
+    Test.@test collect_lines(function_body) == [[]]
+
+
+    function_body = Base.remove_linenums!(quote
+        @assert y == 1
+        x = 1
+        @assert y == 1
+        @assert x == 1
+    end)
+
+    Test.@test collect_assertions(function_body) == [[:(y == 1)], [:(y == 1), :(x == 1)]]
+    Test.@test collect_lines(function_body) == [[:(x = 1)]]
+
+
+    function_body = Base.remove_linenums!(quote
+        @assert y == 1
+        x = 1
+        @assert y == 1
+        x = 1
+        @assert x == 1
+    end)
+
+    Test.@test collect_assertions(function_body) == [[:(y == 1)], [:(y == 1)], [:(x == 1)]]
+    Test.@test collect_lines(function_body) == [[:(x = 1)], [:(x = 1)]]
+
+    function_body = Base.remove_linenums!(quote
+        @assert x == 1
+        @assert y == 1
+        x = 1
+        @assert y == 1
+        x = 1
+        @assert x == 1
+    end)
+
+    Test.@test collect_assertions(function_body) == [[:(x == 1), :(y == 1)], [:(y == 1)], [:(x == 1)]]
+    Test.@test collect_lines(function_body) == [[:(x = 1)], [:(x = 1)]]
+
+end
+
+Test.@testset "Test propagate_formulas" begin
+    formulas = [[]]
+    programs = []
+    Test.@test formulas == [[]]
+
+    formulas = [[], [Equal(DlSymbol(:x), DlReal(1))], []]
+    programs = [Empty(), Empty(), Empty()]
+
+    Test.@test propagate_formulas(formulas, programs) == [[], [Equal(DlSymbol(:x), DlReal(1))], [Equal(DlSymbol(:x), DlReal(1))]]
+
+    formulas = [[], [Equal(DlSymbol(:x), DlReal(1))], []]
+    programs = [Empty(), Assignment(DlSymbol(:x), DlReal(1)), Empty()]
+
+    Test.@test propagate_formulas(formulas, programs) == [[], [Equal(DlSymbol(:x), DlReal(1))], []]
 end
